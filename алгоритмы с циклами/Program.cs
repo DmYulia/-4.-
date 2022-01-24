7e2f8da546bc218476aa07633cd2f3af8e7d4252
Юлия <@LAPTOP-B0K7S1UE>
2022-01-24 18:36:53 +0700

    Добавьте файлы проекта.

---
+++ алгоритмы с циклами/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_Введите целое число больше нуля..");
+            double n;
+
+            //ввод данных
+            int i = 0;
+            do
+            {
+                n = Convert.ToDouble(Console.ReadLine());
+
+                if ((n <= 0) | ((n % 1) != 0))
+                {
+                    Console.WriteLine("_Ошибка ввода данных! Попробуйте еще раз..");
+                }
+                else
+                    i++;
+            }
+            while (i == 0);
+
+            //приведение к int
+            int N = (int)n;
+            int N2 = 0;
+
+            //вычисление квадрата N
+            i = 1;
+            int j = 1;
+            do
+            {
+                N2 += i;
+                Console.WriteLine($"_{j}^2 = {N2}");
+                j++;
+                i += 2;
+
+            }
+            while (i <= (2 * N - 1));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ алгоритмы с циклами/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("алгоритмы с циклами")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("алгоритмы с циклами")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f4807a1a-0252-4fe6-9fb6-b7d2b750de7d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
